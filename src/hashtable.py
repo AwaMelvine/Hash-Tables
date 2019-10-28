97180c815cd7a976ef7f102782aed6d90c842566
AwaMelvine <melvineawa@gmail.com>
2019-10-28 15:21:06 +0100

    Implement insert, retrieve and resize methods

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,21 +1,25 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
+        self.count = 0
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -25,7 +29,6 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
@@ -34,14 +37,12 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
         return self._hash(key) % self.capacity
-
 
     def insert(self, key, value):
         '''
@@ -51,9 +52,12 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        if self.count >= self.capacity:
+            self.resize()
 
-
+        new_item = LinkedPair(key, value)
+        self.storage[self.count] = new_item
+        self.count += 1
 
     def remove(self, key):
         '''
@@ -65,7 +69,6 @@ class HashTable:
         '''
         pass
 
-
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
@@ -74,8 +77,10 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        for n in self.storage:
+            if key == n.key:
+                return n.value
+        return None
 
     def resize(self):
         '''
@@ -84,7 +89,9 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        
+        self.capacity *= 2
+        self.storage.extend([None] * self.capacity)
 
 
 
